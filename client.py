ffdcfbf0513f6d8422a2a7d360d587dc0aa715d3
facumartin <facundo.martin.9@gmail.com>
2023-04-16 19:29:25 -0300

    changes

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
-from datetime import datetime, timedelta
-import requests
-from typing import Dict, List, TypeVar, Union
-from splight_lib.logging import getLogger
 import urllib.parse
+from datetime import datetime, timedelta
+from typing import Dict, List, TypeVar, Union
+
+import requests
+from splight_lib.logging import getLogger
 
 logger = getLogger()
 
@@ -16,13 +17,13 @@ class SolarEdgeAPIClient:
     def get_power(self, site_id: str, start_time: datetime, end_time: datetime) -> Dict:
         url = f"{self._BASE_URL}/site/{site_id}/power/"
 
-        start_time_str = start_time.strftime('%Y-%m-%d %H:%M:%S')
-        end_time_str = end_time.strftime('%Y-%m-%d %H:%M:%S')
+        start_time_str = start_time.strftime("%Y-%m-%d %H:%M:%S")
+        end_time_str = end_time.strftime("%Y-%m-%d %H:%M:%S")
 
         params = {
             "api_key": self._api_key,
             "startTime": start_time_str,
-            "endTime": end_time_str
+            "endTime": end_time_str,
         }
 
         try:
@@ -32,17 +33,19 @@ class SolarEdgeAPIClient:
         except requests.exceptions.HTTPError as err:
             return {}
 
-    def get_energy(self, site_id: str, start_date: datetime, end_date: datetime) -> Dict:
+    def get_energy(
+        self, site_id: str, start_date: datetime, end_date: datetime
+    ) -> Dict:
         url = f"{self._BASE_URL}/site/{site_id}/energy/"
 
-        start_date_str = start_date.strftime('%Y-%m-%d')
-        end_date_str = end_date.strftime('%Y-%m-%d')
+        start_date_str = start_date.strftime("%Y-%m-%d")
+        end_date_str = end_date.strftime("%Y-%m-%d")
 
         params = {
             "api_key": self._api_key,
             "startDate": start_date_str,
             "endDate": end_date_str,
-            "timeUnit": "QUARTER_OF_AN_HOUR"
+            "timeUnit": "QUARTER_OF_AN_HOUR",
         }
 
         try:
@@ -52,13 +55,15 @@ class SolarEdgeAPIClient:
         except requests.exceptions.HTTPError as err:
             return {}
 
-    def get_inverter_data(self, site_id: str, inverter_id: str, start_date: datetime, end_date: datetime) -> Dict:
+    def get_inverter_data(
+        self, site_id: str, inverter_id: str, start_date: datetime, end_date: datetime
+    ) -> Dict:
         url = f"{self._BASE_URL}/site/{site_id}/energy/"
 
         params = {
             "api_key": self._api_key,
             "startTime": start_date,
-            "endTime": end_date
+            "endTime": end_date,
         }
 
         try:
